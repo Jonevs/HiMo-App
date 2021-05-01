589bfd2ad3fe2ba2b1a8be3bc9b41cb34fbb08e6
Jonevs <jonathanevansampurna737@gmail.com>
2021-05-01 20:30:53 +0700

    First Commit

---
+++ Assets/CanvasTransisi.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasTransisi : MonoBehaviour
+{
+    public static string NamaScene;
+
+    public void btn_pindah(string nama)
+    {
+        this.gameObject.SetActive(true);
+        NamaScene = nama;
+        GetComponent<Animator>().Play("end");
+    }
+
+    public void Object_InActive()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void Pindah_Scene()
+    {
+        SceneManager.LoadScene(NamaScene);
+    }
+
+    public void btn_keluargame()
+    {
+        Application.Quit();
+    }
+}
+ 
